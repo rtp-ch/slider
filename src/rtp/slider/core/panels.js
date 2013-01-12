9f81b1079e3f860e28ecdaeb2ab446632cf2d315
Marcel Greter <marcel.greter@ocbnet.ch>
2013-01-13 00:18:51 +0100

    [TASK] Refactors panels ready event

--- src/rtp/slider/core/panels.js
+++ src/rtp/slider/core/panels.js
@@ -120,44 +120,6 @@
 	// @@@ EO method: getPanelSize @@@
 
 
-	// @@@ method: updatePanels @@@
-	// create slidepanels lookup array
-	// trigger updatedPanels hook
-	prototype.updatePanels = function ()
-	{
-
-		// initialize slidepanels
-		this.slidepanels = [];
-
-		// get slides length
-		this.slen = this.slides.length;
-
-		// max index for real slider panels (not cloned ones)
-		this.smax = this.smin + this.slen - 1;
-
-		// test how much viewable each panel is right now
-		for(var i = 0; i < this.panels.length; i ++)
-		{
-
-			// normalize from panel to slide
-			var slide = this.panel2slide(i);
-
-			// generate slidepanels array
-			if (!this.slidepanels[slide])
-			{ this.slidepanels[slide] = [i]; }
-			else { this.slidepanels[slide].push(i); }
-
-		}
-		// EO foreach panel
-
-		// trigger updatedPanels hook
-		// hook is not yet used by anyone
-		// this.trigger('updatedPanels');
-
-	}
-	// @@@ EO method: updatePanels @@@
-
-
 	// @@@ private fn: readPanelsSize @@@
 	function readPanelsSize (invert)
 	{
@@ -233,28 +195,6 @@
 
 	}
 	// @@@ EO private fn: readPanelsStyles @@@
-
-	// @@@ method: checkPanelStyles @@@
-	// get the panel styles from UA
-	// store values to use them later
-	// trigger changedPanelsDim hook
-	// trigger changedPanelsOpp hook
-	prototype.checkPanelsStyle = function(flag)
-	{
-
-		// read styles for both axes
-		readPanelsStyles.call(this, 0);
-		readPanelsStyles.call(this, 1);
-
-		// read some more styles
-		readPanelsSize.call(this, 0);
-		readPanelsSize.call(this, 1);
-
-		// calculate offsets
-		this.updatePanelsOffset();
-
-	}
-	// @@@ EO checkPanelStyles @@@
 
 
 	// @@@ method: readPanelsDim @@@
@@ -373,15 +313,43 @@
 	prototype.plugin('ready', function ()
 	{
 
-		// calculate base values
-		this.updatePanels();
+		// initialize slidepanels
+		this.slidepanels = [];
 
-		// read panel sizes and margins
-		this.checkPanelsStyle();
+		// get slides length
+		this.slen = this.slides.length;
+
+		// max index for real slider panels (not cloned ones)
+		this.smax = this.smin + this.slen - 1;
+
+		// test how much viewable each panel is right now
+		for(var i = 0; i < this.panels.length; i ++)
+		{
+
+			// normalize from panel to slide
+			var slide = this.panel2slide(i);
+
+			// generate slidepanels array
+			if (!this.slidepanels[slide])
+			{ this.slidepanels[slide] = [i]; }
+			else { this.slidepanels[slide].push(i); }
+
+		}
+		// EO foreach panel
+
+		// read styles for both axes
+		readPanelsStyles.call(this, 0);
+		readPanelsStyles.call(this, 1);
+
+		// read some more styles
+		readPanelsSize.call(this, 0);
+		readPanelsSize.call(this, 1);
+
+		// calculate offsets
+		this.updatePanelsOffset();
 
 	}, - 99);
 	// @@@ EO plugin: ready @@@
-
 
 
 // EO extend class prototype
