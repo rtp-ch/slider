6a9afc06f119f1d07ca50067fdec6fc112d5c736
Marcel Greter <marcel.greter@ocbnet.ch>
2013-10-18 03:44:15 +0200

    [BUGFIX] Fixes bug in linux webmerge compiler script

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-perl "`dirname $0`"/webmerge/webmerge.pl -f "`dirname $0`"/conf/slider.conf.xml "$@"
+perl "`dirname $0`"/webmerge/webmerge.sh -f "`dirname $0`"/conf/slider.conf.xml "$@"
