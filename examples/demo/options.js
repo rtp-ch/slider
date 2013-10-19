5117240c2c249a558547f2e052fa315f95277076
Marcel Greter <marcel.greter@ocbnet.ch>
2013-10-19 09:57:57 +0200

    Adds updated demo with configurator

---
+++ examples/demo/options.js
@@ -0,0 +1,55 @@
+var blocks = [
+	'swipe',
+	'tiles',
+	'autoVpOpp',
+	'autoslide'
+];
+
+var texts = [
+	'align',
+	'alignPanelDim',
+	'alignPanelOpp',
+	'alignViewport',
+	'autoVpOppDeadZone',
+	'autoVpOppLifeZone',
+	'slideFirst',
+	'panelsVisible',
+	'tileRows',
+	'tileCols',
+	'tileRowsAtOnce',
+	'tileColsAtOnce',
+	'autoslideDelay',
+	'autoslideAction',
+	'autoslideFirstDelay',
+	'autoslideResumeDelay'
+];
+
+var selects = [
+	'sizerDim',
+	'sizerOpp'
+];
+
+var checkboxes = [
+	'fluidPanelsDim',
+	'fluidPanelsOpp',
+	'swipe',
+	'tiles',
+	'touchSwipe',
+	'mouseSwipe',
+	'gestureSwipe',
+	'progressBar',
+	'carousel',
+	'carousel3d',
+	'vertical',
+	'autoVpOpp',
+	'autoslide',
+	'navDots',
+	'navArrows',
+	'navToolbar',
+	'navKeyboard',
+	'fillViewport',
+	'shrinkViewport',
+	'autoslidePauseOnHover',
+	'autoslideStopOnHover',
+	'autoslideStopOnAction'
+];
